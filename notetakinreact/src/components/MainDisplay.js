6ab10012d398f68d71ac8673f70d873100f1bb12
Abraham Bueno <abueno@localhost.localdomain>
2018-08-08 00:00:06 -0500

    Changes to frontend

---
+++ notetakinreact/src/components/MainDisplay.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+// import moduleName from 'module'
+class MainDisplay extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  componentDidMount = () => {
+    this.getAllNotes()
+  }
+
+  getAllNotes = () => {
+    const token = "Token 9d7ed1c1b05ce411ba5dd1184e3cffe307d10937";
+      // const data = {
+
+      //   'Id': localStorage.getItem('ID')
+      // }
+
+      const optionTwo = {
+        method: 'GET',
+        headers: { 'content-type': 'application/json', 'Authorization': token },
+        // data: data,
+        url: 'https://sprintdjango.herokuapp.com',
+      }
+      
+      axios(optionTwo)
+        .then((resp) => {
+          // localStorage.setItem('notes', resp.data)
+          // dispatch({
+          //   type: GET_NOTES_ACTION,
+          //   payload: resp.data
+          // })
+          console.log(resp)
+          // console.log(resp.data)
+        })
+        .catch(err => console.log(err));
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello from MainDisplay</h1>
+      </div>
+    )
+  }
+}
+
+export default MainDisplay;
